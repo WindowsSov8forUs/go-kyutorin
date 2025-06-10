e99675743849085b3b2b1d5108ba6d20a0c0d30c
WindowsSov8forUs <qwertyuiop2333@hotmail.com>
2025-06-10 22:34:09 +0800

    feat: 包装了 color 库

---
+++ log/color.go
@@ -0,0 +1,142 @@
+package log
+
+import (
+	"fmt"
+
+	"github.com/fatih/color"
+)
+
+// 定义颜色函数类型
+type ColorFunc func(format string, a ...interface{}) string
+
+// 基础颜色函数
+var (
+	// 文本颜色
+	Red     ColorFunc = color.New(color.FgRed).Sprintf
+	Green   ColorFunc = color.New(color.FgGreen).Sprintf
+	Yellow  ColorFunc = color.New(color.FgYellow).Sprintf
+	Blue    ColorFunc = color.New(color.FgBlue).Sprintf
+	Magenta ColorFunc = color.New(color.FgMagenta).Sprintf
+	Cyan    ColorFunc = color.New(color.FgCyan).Sprintf
+	White   ColorFunc = color.New(color.FgWhite).Sprintf
+	Black   ColorFunc = color.New(color.FgBlack).Sprintf
+
+	// 高亮颜色
+	HiRed     ColorFunc = color.New(color.FgHiRed).Sprintf
+	HiGreen   ColorFunc = color.New(color.FgHiGreen).Sprintf
+	HiYellow  ColorFunc = color.New(color.FgHiYellow).Sprintf
+	HiBlue    ColorFunc = color.New(color.FgHiBlue).Sprintf
+	HiMagenta ColorFunc = color.New(color.FgHiMagenta).Sprintf
+	HiCyan    ColorFunc = color.New(color.FgHiCyan).Sprintf
+	HiWhite   ColorFunc = color.New(color.FgHiWhite).Sprintf
+	HiBlack   ColorFunc = color.New(color.FgHiBlack).Sprintf
+
+	// 加粗颜色
+	BoldRed     ColorFunc = color.New(color.FgRed, color.Bold).Sprintf
+	BoldGreen   ColorFunc = color.New(color.FgGreen, color.Bold).Sprintf
+	BoldYellow  ColorFunc = color.New(color.FgYellow, color.Bold).Sprintf
+	BoldBlue    ColorFunc = color.New(color.FgBlue, color.Bold).Sprintf
+	BoldMagenta ColorFunc = color.New(color.FgMagenta, color.Bold).Sprintf
+	BoldCyan    ColorFunc = color.New(color.FgCyan, color.Bold).Sprintf
+	BoldWhite   ColorFunc = color.New(color.FgWhite, color.Bold).Sprintf
+
+	// 日志级别专用颜色
+	FatalColor ColorFunc = color.New(color.FgHiRed, color.Bold).Sprintf // 深红色加粗
+	PanicColor ColorFunc = color.New(color.FgHiRed, color.Bold).Sprintf // 深红色加粗
+	ErrorColor ColorFunc = color.New(color.FgRed).Sprintf               // 红色
+	WarnColor  ColorFunc = color.New(color.FgHiYellow).Sprintf          // 橙色（高亮黄色）
+	InfoColor  ColorFunc = color.New(color.FgBlue).Sprintf              // 蓝色
+	DebugColor ColorFunc = color.New(color.FgYellow).Sprintf            // 黄色
+	TraceColor ColorFunc = color.New(color.FgGreen).Sprintf             // 绿色
+	TimeColor  ColorFunc = color.New(color.FgHiCyan).Sprintf            // 时间戳颜色（浅蓝色）
+)
+
+// 便捷函数 - 直接输出彩色文本（不换行）
+func PrintRed(text string) {
+	fmt.Print(Red("%s", text))
+}
+
+func PrintGreen(text string) {
+	fmt.Print(Green("%s", text))
+}
+
+func PrintYellow(text string) {
+	fmt.Print(Yellow("%s", text))
+}
+
+func PrintBlue(text string) {
+	fmt.Print(Blue("%s", text))
+}
+
+func PrintCyan(text string) {
+	fmt.Print(Cyan("%s", text))
+}
+
+// 便捷函数 - 直接输出彩色文本（带换行）
+func PrintlnRed(text string) {
+	fmt.Println(Red("%s", text))
+}
+
+func PrintlnGreen(text string) {
+	fmt.Println(Green("%s", text))
+}
+
+func PrintlnYellow(text string) {
+	fmt.Println(Yellow("%s", text))
+}
+
+func PrintlnBlue(text string) {
+	fmt.Println(Blue("%s", text))
+}
+
+func PrintlnCyan(text string) {
+	fmt.Println(Cyan("%s", text))
+}
+
+// 格式化输出函数
+func PrintfRed(format string, a ...interface{}) {
+	fmt.Print(Red(format, a...))
+}
+
+func PrintfGreen(format string, a ...interface{}) {
+	fmt.Print(Green(format, a...))
+}
+
+func PrintfYellow(format string, a ...interface{}) {
+	fmt.Print(Yellow(format, a...))
+}
+
+func PrintfBlue(format string, a ...interface{}) {
+	fmt.Print(Blue(format, a...))
+}
+
+func PrintfCyan(format string, a ...interface{}) {
+	fmt.Print(Cyan(format, a...))
+}
+
+// 获取日志级别对应的颜色函数
+func GetLevelColorFunc(level string) ColorFunc {
+	switch level {
+	case "FATAL":
+		return FatalColor
+	case "PANIC":
+		return PanicColor
+	case "ERROR":
+		return ErrorColor
+	case "WARN":
+		return WarnColor
+	case "INFO":
+		return InfoColor
+	case "DEBUG":
+		return DebugColor
+	case "TRACE":
+		return TraceColor
+	default:
+		return White
+	}
+}
+
+// 创建自定义颜色函数
+func NewColorFunc(attrs ...color.Attribute) ColorFunc {
+	return color.New(attrs...).Sprintf
+}
--- log/log.go
+++ log/log.go
@@ -2,12 +2,12 @@ package log
 
 import (
 	"fmt"
-	"log"
+	"io"
 	"os"
 	"sync"
 	"time"
 
-	"github.com/fatih/color"
+	"github.com/sirupsen/logrus"
 )
 
 // Logger 日志等级
@@ -26,18 +26,81 @@ const (
 
 // Logger 日志结构体
 type Logger struct {
-	FileLogger *log.Logger
-	Mutex      sync.Mutex
-	Level      LogLevel
+	*logrus.Logger
+	Mutex sync.Mutex
+	Level LogLevel
 }
 
 // 内部 Logger 对象
 var logger Logger
 
+// CustomFormatter 自定义格式化器
+type CustomFormatter struct {
+	TimestampFormat string
+	ForceColors     bool
+}
+
+// Format 实现 logrus.Formatter 接口
+func (f *CustomFormatter) Format(entry *logrus.Entry) ([]byte, error) {
+	// 时间戳颜色（浅蓝色）
+	timestamp := HiCyan("[%s]", entry.Time.Format(f.TimestampFormat))
+
+	// 日志级别颜色和名称
+	var levelColor ColorFunc
+	var levelName string
+	switch entry.Level {
+	case logrus.PanicLevel:
+		levelColor = PanicColor // 深红色（与 Fatal 相同）
+		levelName = "PANIC"
+	case logrus.FatalLevel:
+		levelColor = FatalColor // 深红色
+		levelName = "FATAL"
+	case logrus.ErrorLevel:
+		levelColor = ErrorColor // 红色
+		levelName = "ERROR"
+	case logrus.WarnLevel:
+		levelColor = WarnColor // 橙色（高亮黄色，比普通黄色更深）
+		levelName = "WARN"
+	case logrus.InfoLevel:
+		levelColor = InfoColor // 蓝色
+		levelName = "INFO"
+	case logrus.DebugLevel:
+		levelColor = DebugColor // 黄色（普通黄色，比高亮黄色浅）
+		levelName = "DEBUG"
+	case logrus.TraceLevel:
+		levelColor = TraceColor // 绿色
+		levelName = "TRACE"
+	default:
+		levelColor = White
+		levelName = "UNKNOWN"
+	}
+
+	level := levelColor("[%s]", levelName)
+
+	// 组合日志消息
+	return []byte(fmt.Sprintf("%s %s: %s\n", timestamp, level, entry.Message)), nil
+}
+
 // 初始化 Logger 对象
 func init() {
-	logger.FileLogger = log.New(getLogFile(), "", 0)
+	logger.Logger = logrus.New()
+
+	// 设置自定义日志格式
+	logger.SetFormatter(&CustomFormatter{
+		TimestampFormat: "2006-01-02 15:04:05",
+		ForceColors:     true,
+	})
+
+	// 配置多输出（控制台和文件）
+	logFile := getLogFile()
+	if logFile != nil {
+		logger.SetOutput(io.MultiWriter(os.Stdout, logFile))
+	} else {
+		logger.SetOutput(os.Stdout)
+	}
+
 	logger.Level = INFO
+	logger.SetLevel(logrus.InfoLevel)
 }
 
 // getLogFile 获取当前 .log 文件对象
@@ -59,55 +122,30 @@ func getLogFile() *os.File {
 	return file
 }
 
-// getTimestamp 获取当前时间
-func getTimestamp() string {
-	timestamp := time.Now().Format("2006-01-02 15:04:05")
-	return fmt.Sprintf("[%s]", timestamp)
-}
-
-// getColor 获取当前日志等级颜色
-func getColor(level LogLevel) *color.Color {
+// convertLogLevel 转换自定义日志级别到 Logrus 级别
+func convertLogLevel(level LogLevel) logrus.Level {
 	switch level {
 	case FATAL:
-		return color.New(color.FgHiRed)
+		return logrus.FatalLevel
 	case ERROR:
-		return color.New(color.FgRed)
+		return logrus.ErrorLevel
 	case WARN:
-		return color.New(color.FgYellow)
+		return logrus.WarnLevel
 	case INFO:
-		return color.New(color.FgWhite)
+		return logrus.InfoLevel
 	case DEBUG:
-		return color.New(color.FgHiYellow)
+		return logrus.DebugLevel
 	case TRACE:
-		return color.New(color.FgGreen)
+		return logrus.TraceLevel
 	default:
-		return color.New(color.FgWhite)
-	}
-}
-
-// getLevelName 获取日志等级名称
-func getLevelName(level LogLevel) string {
-	switch level {
-	case FATAL:
-		return "FATAL"
-	case ERROR:
-		return "ERROR"
-	case WARN:
-		return "WARN"
-	case INFO:
-		return "INFO"
-	case DEBUG:
-		return "DEBUG"
-	case TRACE:
-		return "TRACE"
-	default:
-		return "UNKNOWN"
+		return logrus.InfoLevel
 	}
 }
 
 // SetLogLevel 设置 Logger 等级
 func SetLogLevel(level LogLevel) {
 	logger.Level = level
+	logger.SetLevel(convertLogLevel(level))
 }
 
 // GetLogger 获取 Logger 对象
@@ -116,128 +154,121 @@ func GetLogger() *Logger {
 }
 
 // Println 打印日志
-func (logger *Logger) Println(level LogLevel, v ...interface{}) {
+func (l *Logger) Println(level LogLevel, v ...interface{}) {
 	// 判断日志等级
-	if level > logger.Level {
+	if level > l.Level {
 		return
 	}
-	// 获取当前时间
-	timestamp := getTimestamp()
-	// 获取当前日志等级颜色
-	color := getColor(level)
-	// 获取日志内容
-	content := fmt.Sprintf("%s [%s]: %s", timestamp, getLevelName(level), fmt.Sprintln(v...))
-	// 写入日志
-	logger.Mutex.Lock()
-	defer logger.Mutex.Unlock()
-	color.Printf("%s", content)
-	logger.FileLogger.Printf(content)
+
+	l.Mutex.Lock()
+	defer l.Mutex.Unlock()
+
+	logrusLevel := convertLogLevel(level)
+	l.Logger.Log(logrusLevel, v...)
 }
 
 // Fatal 致命错误
 func Fatal(v ...interface{}) {
-	logger.Println(FATAL, v...)
-	os.Exit(1)
+	logger.Fatal(v...)
 }
 
 // Fatalf 致命错误
 func Fatalf(format string, v ...interface{}) {
-	logger.Println(FATAL, fmt.Sprintf(format, v...))
-	os.Exit(1)
+	logger.Fatalf(format, v...)
 }
 
 // logger 实例的 Error 方法
-func (logger *Logger) Error(v ...interface{}) {
-	logger.Println(DEBUG, v...)
+func (l *Logger) Error(v ...interface{}) {
+	l.Logger.Error(v...)
 }
 
 // Error 错误
 func Error(v ...interface{}) {
-	logger.Println(ERROR, v...)
+	logger.Error(v...)
 }
 
 // logger 实例的 Errorf 方法
-func (logger *Logger) Errorf(format string, v ...interface{}) {
-	logger.Println(DEBUG, fmt.Sprintf(format, v...))
+func (l *Logger) Errorf(format string, v ...interface{}) {
+	l.Logger.Errorf(format, v...)
 }
 
 // Errorf 错误
 func Errorf(format string, v ...interface{}) {
-	logger.Println(ERROR, fmt.Sprintf(format, v...))
+	logger.Errorf(format, v...)
 }
 
 // logger 实例的 Warn 方法
-func (logger *Logger) Warn(v ...interface{}) {
-	logger.Println(DEBUG, v...)
+func (l *Logger) Warn(v ...interface{}) {
+	l.Logger.Warn(v...)
 }
 
 // Warn 警告
 func Warn(v ...interface{}) {
-	logger.Println(WARN, v...)
+	logger.Warn(v...)
 }
 
 // logger 实例的 Warnf 方法
-func (logger *Logger) Warnf(format string, v ...interface{}) {
-	logger.Println(DEBUG, fmt.Sprintf(format, v...))
+func (l *Logger) Warnf(format string, v ...interface{}) {
+	l.Logger.Warnf(format, v...)
 }
 
 // Warnf 警告
 func Warnf(format string, v ...interface{}) {
-	logger.Println(WARN, fmt.Sprintf(format, v...))
+	logger.Warnf(format, v...)
 }
 
 // logger 实例的 Info 方法
-func (logger *Logger) Info(v ...interface{}) {
-	logger.Println(DEBUG, v...)
+func (l *Logger) Info(v ...interface{}) {
+	l.Logger.Info(v...)
 }
 
 // Info 信息
 func Info(v ...interface{}) {
-	logger.Println(INFO, v...)
+	logger.Info(v...)
 }
 
 // logger 实例的 Infof 方法
-func (logger *Logger) Infof(format string, v ...interface{}) {
-	logger.Println(DEBUG, fmt.Sprintf(format, v...))
+func (l *Logger) Infof(format string, v ...interface{}) {
+	l.Logger.Infof(format, v...)
 }
 
 // Infof 信息
 func Infof(format string, v ...interface{}) {
-	logger.Println(INFO, fmt.Sprintf(format, v...))
+	logger.Infof(format, v...)
 }
 
 // logger 实例的 Debug 方法
-func (logger *Logger) Debug(v ...interface{}) {
-	logger.Println(DEBUG, v...)
+func (l *Logger) Debug(v ...interface{}) {
+	l.Logger.Debug(v...)
 }
 
 // Debug 调试
 func Debug(v ...interface{}) {
-	logger.Println(DEBUG, v...)
+	logger.Debug(v...)
 }
 
 // logger 实例的 Debugf 方法
-func (logger *Logger) Debugf(format string, v ...interface{}) {
-	logger.Println(DEBUG, fmt.Sprintf(format, v...))
+func (l *Logger) Debugf(format string, v ...interface{}) {
+	l.Logger.Debugf(format, v...)
 }
 
 // Debugf 调试
 func Debugf(format string, v ...interface{}) {
-	logger.Println(DEBUG, fmt.Sprintf(format, v...))
+	logger.Debugf(format, v...)
 }
 
 // Trace 追踪
 func Trace(v ...interface{}) {
-	logger.Println(TRACE, v...)
+	logger.Trace(v...)
 }
 
 // Tracef 追踪
 func Tracef(format string, v ...interface{}) {
-	logger.Println(TRACE, fmt.Sprintf(format, v...))
+	logger.Tracef(format, v...)
 }
 
-// Sync 未实现的方法
-func (logger *Logger) Sync() error {
-	// TODO: 这个方法是做什么的？目前只是用来实现接口
+// Sync 同步日志缓冲区
+func (l *Logger) Sync() error {
+	// Logrus 会自动同步，但为了兼容性保留此方法
 	return nil
 }
